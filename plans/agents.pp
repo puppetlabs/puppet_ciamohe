1745753b4322cbcc918398d6e4ecaabe949e74ac
tphoney <tp@puppet.com>
2019-07-04 14:32:45 +0100

    manifest per node, and cleanup

--- plans/agents.pp
+++ plans/agents.pp
@@ -7,7 +7,7 @@ plan puppet_ciamohe::agents(
 
 
   # install agents
-  run_task('puppet_agent::install', 'localhost', action => 'provision', inventory => '/Users/tp/workspace/git/puppet_ciamohe')
+  run_task('puppet_agent::install', $agents, action => 'provision', inventory => '/Users/tp/workspace/git/puppet_ciamohe')
   # run the cert signing dance
   # run agent -t
 }
