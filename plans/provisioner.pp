ecbd20a2da93f6bfb14694c32a53fd388c76dcde
Erick Banks <erick@puppetlabs.com>
2019-07-11 18:28:42 -0700

    Fix cert signing on windows and test against linux and windows

--- plans/provisioner.pp
+++ plans/provisioner.pp
@@ -5,5 +5,5 @@ plan puppet_ciamohe::provisioner(
   run_task('provision::vmpooler', 'localhost', action => 'provision', platform => 'centos-7-x86_64', inventory => './', vars => 'role: pe')
   # provision agents  agent_linux agent_windows
   run_task('provision::vmpooler', 'localhost', action => 'provision', platform => 'centos-6-x86_64', inventory => './', vars => 'role: agent_linux')
-#  run_task('provision::vmpooler', 'localhost', action => 'provision', platform => 'win-2016-x86_64', inventory => './', vars => 'role: agent_windows')
+  run_task('provision::vmpooler', 'localhost', action => 'provision', platform => 'win-2016-x86_64', inventory => './', vars => 'role: agent_windows')
 }
--- plans/tester.pp
+++ plans/tester.pp
@@ -3,10 +3,16 @@ plan puppet_ciamohe::tester(
   # get pe_server ?
   $server = get_targets('*').filter |$n| { $n.vars['role'] == 'pe' }
   $agent_linux = get_targets('*').filter |$n| { $n.vars['role'] =~ /agent_linux/ }
-
+  $agent_windows = get_targets('*').filter |$n| { $n.vars['role'] =~ /agent_windows/ }
+  
   # get the agent_name
-  $agent_hostname = $agent_linux[0].name
   $manifest = "class { 'motd':\ncontent => 'foomph'\n}"
-  run_task('puppet_ciamohe::update_node_pp', $server, manifest => $manifest, target_node => $agent_hostname)
-  run_command('puppet agent -t', $agent_linux)
+  
+  $agent_linux_hostname = $agent_linux[0].name
+  run_task('puppet_ciamohe::update_node_pp', $server, manifest => $manifest, target_node => $agent_linux_hostname)
+  #run_command('puppet agent -t', $agent_linux)
+  
+  $agent_windows_hostname = $agent_windows[0].name
+  run_task('puppet_ciamohe::update_node_pp', $server, manifest => $manifest, target_node => $agent_windows_hostname)
+  run_command('puppet agent -t', get_targets('*').filter |$n| { $n.vars['role'] =~ /agent/})
 }
